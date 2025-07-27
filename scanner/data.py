5e9467d6d9cedd4695f2946eafb65b9549a6e0c2
unico482 <gxc29@proton.me>
2025-07-27 01:41:38 -0500

    Update data.py

--- scanner/data.py
+++ scanner/data.py
@@ -1,12 +1,12 @@
 # scanner/data.py
 
 from scanner.alpaca_data import fetch_stock_bars
-from scanner.binance_spot_data import fetch_bars as fetch_crypto_bars
+from scanner.binance_spot_data import fetch_bars as fetch_crypto_bars   
 
 def fetch_bars(tickers, timeframe: str, watchlist_type: str):
     if watchlist_type == "stock":
         return fetch_stock_bars(tickers, timeframe)
     elif watchlist_type == "crypto":
-        return fetch_crypto_bars_binance_futures(tickers, timeframe)
+        return fetch_crypto_bars(tickers, timeframe)                  
     else:
         raise ValueError("Invalid watchlist type")
